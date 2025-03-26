a8d05624d163fa088a664c0d3c0f0cf0f56648a8
zhanghualong <138177671+Z-hualong@users.noreply.github.com>
2025-03-26 20:28:33 +0800

    Delete 2.segmentation/configs/pspnet_oeem directory

--- 2.segmentation/configs/pspnet_oeem/pspnet_wres38-d8_10k_histo_test.py
+++
@@ -1,3 +0,0 @@
-_base_ = './pspnet_wres38-d8_10k_histo.py'
-test_cfg=dict(mode='whole', crop_size=(224, 224),crf=False,pred_out_path='F:/data/data_all/weak_suprvised_data/LUAD-HistoSeg/LUAD-HistoSeg/WSI/wsi_cut/405723/3200_2240/pred/')
-
